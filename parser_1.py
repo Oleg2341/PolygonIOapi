c4a9ea874377ee4f2728fff036a8c4bfeaba5903
Oleg2341 <128373650+Oleg2341@users.noreply.github.com>
2024-06-24 14:31:41 +0200

    Add files via upload

--- parser_1.py
+++ parser_1.py
@@ -7,55 +7,67 @@ from pandas import DataFrame, to_datetime
 
 
 class Unit(str, Enum):
+    """
+    Enum class representing time units for data retrieval.
+    """
     DAY = 'day'
     HOUR = 'hour'
     MINUTE = 'minute'
 
 
 class PolygonAPI:
+    """
+    Class for interacting with the Polygon.io API to fetch financial market data.
+    """
     DOMAIN: str = 'https://api.polygon.io/'
     API_KEY: str = 'apiKey=HLq0CJZsrQubZCXx5g64t23NJprSI8rn'
 
     def __init__(self):
+        """
+        Initialize the PolygonAPI class with the name of the class and valid units for data retrieval.
+        """
         self.name = self.__class__.__name__.__str__()
 
         self.__valid_units = ', '.join([unit.value for unit in Unit.__members__.values()])
 
     def __repr__(self):
+        """
+        Return a string representation of the class.
+        """
         return self.name
 
     __name__ = __str__ = __repr__
 
     @staticmethod
-    def __datetime_to_str(start: datetime, end: datetime) -> (str, str):
+    def __datetime_to_str(start: datetime, end: datetime) -> str:
         """
+        Convert datetime range to string format required by the API.
 
-        :param start:
-        :param end:
-        :return:
+        :param start: Start datetime.
+        :param end: End datetime.
+        :return: String representation of the datetime range.
         """
-
         return f'{start:%Y-%m-%d}/{end:%Y-%m-%d}'
 
     @staticmethod
-    def __timeframe_to_str(interval: int, unit: str) -> (str, str):
+    def __timeframe_to_str(interval: int, unit: str) -> str:
         """
+        Convert interval and unit to string format required by the API.
 
-        :param interval:
-        :param unit:
-        :return:
+        :param interval: Time interval.
+        :param unit: Time unit.
+        :return: String representation of the timeframe.
         """
-
         return f'{interval}/{unit}'
 
     @staticmethod
     def __params_to_str(params) -> str:
         """
+        Convert additional parameters to a query string.
 
-        :param params:
-        :return:
+        :param params: Dictionary of additional parameters.
+        :return: Query string.
         """
-
         link = '?'
         for key, value in params.items():
             link += f'{key}={value}&'
@@ -64,11 +76,11 @@ class PolygonAPI:
     @staticmethod
     def __resp_to_df(resp) -> DataFrame:
         """
+        Convert API response to a pandas DataFrame.
 
-        :param resp:
-        :return:
+        :param resp: API response.
+        :return: DataFrame containing the response data.
         """
-
         if resp is None:
             return DataFrame()
 
@@ -81,10 +93,10 @@ class PolygonAPI:
     @property
     def symbols_link(self) -> str:
         """
+        Construct the API link for fetching available symbols.
 
-        :return:
+        :return: URL string for symbols.
         """
-
         return self.DOMAIN + 'v3/reference/tickers?' + self.API_KEY
 
     def get_candles_link(
@@ -95,19 +107,28 @@ class PolygonAPI:
             **kwargs: str
     ) -> str:
         """
+        Construct the API link for fetching candlestick data.
 
-        :return:
+        :param symbol: Stock symbol.
+        :param tf: Timeframe string.
+        :param time: Time range string.
+        :param kwargs: Additional parameters.
+        :return: URL string for candlestick data.
+        
+        Example:
+        >>> api = PolygonAPI()
+        >>> api.get_candles_link('AAPL', '1/day', '2023-01-01/2023-01-31', adjusted='true')
+        'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-01/2023-01-31?adjusted=true&apiKey=HLq0CJZsrQubZCXx5g64t23NJprSI8rn'
         """
-
         return self.DOMAIN + f'v2/aggs/ticker/{symbol}/range/{tf}/{time}' + \
             self.__params_to_str(kwargs) + self.API_KEY
 
     def get_symbols(self) -> list:
         """
+        Fetch the list of available symbols from the API.
 
-        :return:
+        :return: List of symbols.
         """
-
         with Session() as s:
             return s.get(self.symbols_link).json().get('results')
 
@@ -122,16 +143,23 @@ class PolygonAPI:
             **kwargs: str
     ) -> None | DataFrame | dict:
         """
+        Fetch candlestick data for a given symbol and timeframe.
 
-        :param symbol:
-        :param interval:
-        :param unit:
-        :param start:
-        :param end:
-        :param limit:
-        :return:
+        :param symbol: Stock symbol.
+        :param interval: Time interval.
+        :param unit: Time unit (e.g., 'day', 'hour', 'minute').
+        :param start: Start datetime.
+        :param end: End datetime.
+        :param limit: Maximum number of records to fetch.
+        :param kwargs: Additional parameters.
+        :return: DataFrame containing candlestick data or error message.
+        
+        Example:
+        >>> api = PolygonAPI()
+        >>> start = datetime(2023, 1, 1)
+        >>> end = datetime(2023, 1, 31)
+        >>> df = api.get_klines('AAPL', 1, 'day', start, end, adjusted='true')
         """
-
         try:
             unit = Unit(unit)
         except ValueError:
